12592cb0641341fdba6147115247eb7d851c3c21
3001Aswini <123323057+3001Aswini@users.noreply.github.com>
2024-07-01 20:51:23 +0530

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,12 +3,12 @@ import {getAuth} from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "apikey",
-  authDomain: "medicaux.firebaseapp.com",
-  projectId: "medicaux",
-  storageBucket: "medicaux.appspot.com",
-  messagingSenderId: "654128259945",
-  appId: "1:654128259945:web:978df6f2ef83c4b5b393f8",
-  measurementId: "G-ZP8GCQ7NW6"
+  authDomain: "medicaux-ed0ee.firebaseapp.com",
+  projectId: "medicaux-ed0ee",
+  storageBucket: "medicaux-ed0ee.appspot.com",
+  messagingSenderId: "670873354564",
+  appId: "1:670873354564:web:1ce93761a67fff4b6fd711",
+  measurementId: "G-1LRRHF0NKH"
 };
 
 const app = initializeApp(firebaseConfig);
